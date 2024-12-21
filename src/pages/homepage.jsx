ed67ba948bb383c755ac9231ad87cba9f4b296c4
Viacheslav Sidorov <me@xamelllion.ru>
2024-12-21 15:40:16 +0300

    test

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -57,11 +57,11 @@ const vkFunc = () => {
 			  }).then(response =>response.json())
 			  .then(data => {
 				console.log(data)
+				localStorage.setItem('user', JSON.stringify(data))
 			  })
 		}
 		
 		function vkidOnError(error) {
-			// Обработка ошибки
 			console.log(error)
 		}
 	}
@@ -72,8 +72,15 @@ const Homepage = () => {
 		window.scrollTo(0, 0);
 	}, []);
 
+	const [text, setText] = useState('');
+
 	useEffect(() => {
 		vkFunc()
+		let user_data = localStorage.getItem('user')
+		if (user_data != undefined) {
+			let info = JSON.parse(user_data)
+			setText(`Добрый день ${info.firstname} ${info.lastname}!`)
+		}
 	}, []);
 
 	return (
@@ -81,7 +88,25 @@ const Homepage = () => {
 			<div className="page-content">
 				{/* <NavBar active="home" /> */}
 				<div className="content-wrapper">
-					<div className="authDiv" id="vkAuth"></div>
+					<div className="header">
+						<div className="login_btns">
+							<div className="authDiv" id="vkAuth"></div>
+							<div className="authDiv">
+								<div class="login-container">
+									<a href="https://github.com/login/oauth/authorize" className="github-login-button">
+									<svg className="github-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
+										<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.54 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.01.08-2.1 0 0 .67-.22 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.09.16 1.9.08 2.1.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.015 8.015 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+									</svg>
+									Войти с GitHub
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className="header_user_info">
+							<p>{text}</p>
+						</div>
+					</div>
+					
 					<div className="homepage-logo-container">
 						{/* <div style={logoStyle}>
 							<Logo width={logoSize} link={false} />
