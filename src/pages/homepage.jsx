79e72cfdd5e26019dbe8e251b9278cb74f68f278
Viacheslav Sidorov <me@xamelllion.ru>
2024-12-22 18:25:32 +0300

    test

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -23,7 +23,7 @@ const vkFunc = (setText, setShowExit) => {
 
 		VKID.Config.init({
 			app: 52877645,
-			redirectUrl: 'https://xmllln.ru/vk',
+			redirectUrl: 'https://site.xmllln.ru/vk',
 			responseMode: VKID.ConfigResponseMode.Callback,
 			source: VKID.ConfigSource.LOWCODE,
 			scope: '',
